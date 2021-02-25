096b1e2883735b39b0f9587c12ca0d9e5aff528d
Beam-from-nose <75787902+Beam-from-nose@users.noreply.github.com>
2021-02-26 06:21:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -57,19 +57,7 @@ async def kj(ctx,imno1,imno2=None):
     if state is None: 
         return False
     
-   # members = state.channel.members  
-     members = [
-        '1', 
-        '2', 
-        '3',
-        '4',
-        '5',
-        '6', 
-        '7', 
-        '8',
-        '9',
-        '0'
-    ]
+    members = state.channel.members  
     members_count = len(members) # 人数取得
         
     #人数分の役職
@@ -104,21 +92,21 @@ async def kj(ctx,imno1,imno2=None):
     m = 0
     for member in members:
         if role_list[m] == 1:
-            #await member.send('あなたは狂人です')
-            await ctx.send(あなたは狂人です)
+            await member.send('あなたは狂人です')
+            #await ctx.send(あなたは狂人です)
 
         elif two_mode == True:
             if m == imno1 or m == imno2:
                 if kill_flag == True:
-                    #await member.send('あなたはキルできるインポスター')
-                    await ctx.send(あなたはキルできるインポスター)
+                    await member.send('あなたはキルできるインポスター')
+                    #await ctx.send(あなたはキルできるインポスター)
                     kill_flag = False 
                 else:
-                    #await member.send('あなたはキルできないインポスター')       
-                    await ctx.send(あなたはキルできないインポスター)
+                    await member.send('あなたはキルできないインポスター')       
+                    #await ctx.send(あなたはキルできないインポスター)
                     kill_flag = True 
         else:
-            await ctx.send(member)
+            #await ctx.send(member)
 
         m = m + 1
 
