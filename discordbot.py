05084f001b8577d53ec7b7ef2776cda35848ad00
Beam-from-nose <75787902+Beam-from-nose@users.noreply.github.com>
2021-02-27 18:18:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -195,7 +195,7 @@ async def show2(ctx):
     
 
 @bot.event
-async def on_raw_reaction_add(reaction, user):
+async def on_reaction_add(reaction, user):
 	message = reaction.message
 	emoji = reaction.emoji
 
