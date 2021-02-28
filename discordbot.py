0815825dd8a8bc68f848d21afad68e31ab37c5f4
Beam-from-nose <75787902+Beam-from-nose@users.noreply.github.com>
2021-03-01 03:40:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -47,10 +47,7 @@ async def act(ctx):
 		await member.send(act_list[i])
 		i = i + 1
 
-
-
-@bot.command()
-async def show(ctx):
+def get_member_list(ctx):
 	
 	# メンバーリストを取得
 	state = ctx.author.voice # コマンド実行者のVCステータスを取得
@@ -76,7 +73,19 @@ async def show(ctx):
 		return False
 	
 	message = '\n'.join(messages)
+	
+	return message
+
+	
+@bot.command()
+async def show(ctx):
+	
+	message = get_member_list(ctx)
+	if message == None:
+		return False
+
 	send_message = await ctx.send(message)
+
 	list = [
 		'\N{DIGIT ZERO}\N{COMBINING ENCLOSING KEYCAP}', 
 		'\N{DIGIT ONE}\N{COMBINING ENCLOSING KEYCAP}', 
@@ -91,12 +100,15 @@ async def show(ctx):
 		'\N{BLACK RIGHT-POINTING TRIANGLE}',
 		'\N{NUMBER SIGN}\N{COMBINING ENCLOSING KEYCAP}'
 	]
+	
 	for a in list:
 		await send_message.add_reaction(a)
 	
 
 @bot.event
 async def on_reaction_add(reaction, user):
+
+	print(reaction.emoji)
 
 	#スタート以外はスルー
 	if reaction.emoji != "▶":
@@ -225,112 +237,3 @@ async def on_reaction_add(reaction, user):
 		]
 		for a in list:
 			await message.add_reaction(a)
-
-			
-			
-			
-			
-@bot.command()
-async def kj(ctx,imno1,imno2=None):
-	
-	入力値チェック
-	if len(imno1) != 1:
-		await ctx.send('1つ目の入力値が不正です')
-		return False
-	if imno2 != None:
-		if len(imno2) != 1:
-			await ctx.send('2つ目の入力値が不正です')
-			return False
-		if int(imno1) == int(imno2):
-			await ctx.send('同じ番号が指定されています')
-			return False
-	
-	# メンバーリストを取得
-	state = ctx.author.voice # コマンド実行者のVCステータスを取得
-	if state is None: 
-		return False
-	
-	members = state.channel.members
-	members_count = len(members) # 人数取得
-	
-	#人数チェック
-	if members_count < 3:
-		await ctx.send('ボイスチャンネルの人数が少なすぎます')
-		return False
-	
-	#人数分の役職
-	role_list = []
-	for i in range(members_count):
-		
-		if i == 0:
-			role_list.append(1)
-		else:
-			role_list.append(0)
-	 
-	while True:
-		random.shuffle(role_list)
-		
-		#インポスター1人
-		if imno2 == None:
-			if role_list[int(imno1)] == 0:
-				two_mode = False
-				break
-
-		#インポスター2人
-		else:
-			if role_list[int(imno1)] == 0 and role_list[int(imno2)] == 0:
-				two_mode = True
-				if random.random() >= 0.5:
-					kill_flag = True 
-				else:
-					kill_flag = False
-				break
-
-	m = 0
-	for member in members:
-		if role_list[m] == 1:
-			await member.send('あなたは狂人')
-			#await ctx.send('あなたは狂人')
-		else:
-			if two_mode == True:
-				if m == int(imno1) or m == int(imno2):
-					if kill_flag == True:
-						await member.send('あなたはキルできるインポスター')
-						#await ctx.send('あなたはキルできるインポスター')
-						kill_flag = False 
-					else:
-						await member.send('あなたはキルできないインポスター')
-						#await ctx.send('あなたはキルできないインポスター')
-						kill_flag = True 
-				else:
-					await member.send('あなたはクルー')
-					#await ctx.send('あなたはクルー')
-			else:
-				await member.send('あなたはクルー')
-				#await ctx.send('あなたはクルー')
-
-		m = m + 1
-
-@bot.command()
-async def show2(ctx):
-	
-	# メンバーリストを取得
-	state = ctx.author.voice # コマンド実行者のVCステータスを取得
-	if state is None: 
-		return False
-	
-	members = state.channel.members
-
-	i = 0
-	messages = []
-	for member in members:
-		if member.nick == None:
-			messages.append(str(i) + '-' + member.name)
-		else:
-			messages.append(str(i) + '-' + member.nick)		 
-		i = i + 1
-	
-	message = '\n'.join(messages)
-	await ctx.send(message)			
-			
-bot.run(token)
