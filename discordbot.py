988bfe8b7f829049cd3fc4def4aa04abe53986a2
Beam-from-nose <75787902+Beam-from-nose@users.noreply.github.com>
2021-05-19 01:13:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,6 +17,14 @@ async def on_command_error(ctx, error):
 	error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
 	await ctx.send(error_msg)
 
+@client.event
+async def on_member_join(member):
+    # 用意したIDから Role オブジェクトを取得
+    role = member.guild.get_role(844138721200439338)
+    # 入ってきた Member に役職を付与
+    await member.add_roles(role)
+			
+	
 @bot.command()
 async def act(ctx):
 
