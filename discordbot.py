80aa3f25930cf793ba5bf500ff9baca694e19afa
Beam-from-nose <75787902+Beam-from-nose@users.noreply.github.com>
2021-05-21 04:22:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -62,13 +62,15 @@ async def tes(ctx):
 	await ctx.message.delete()
 
 # 300秒に一回ループ
-#@tasks.loop(seconds=300)
-#async def loop():	
-#	client = discord.Client()
-#	guild = client.get_guild(799680125024337950)
-#	channel = guild.get_channel(844220827369209857)
-#	await channel.send('/tes')
-#loop.start()
+@tasks.loop(seconds=30)
+async def loop():	
+
+	guild = bot.client.get_guild(799680125024337950)
+	print(guild)
+	channel = guild.get_channel(844220827369209857)
+	print(channel)
+	#await channel.send('/tes')
+loop.start()
 
 @bot.command()
 async def act(ctx):
