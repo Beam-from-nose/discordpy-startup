6e41a4ac8e0ed33f449de28c85dbf5630ba86c81
Beam-from-nose <75787902+Beam-from-nose@users.noreply.github.com>
2021-05-20 22:05:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,6 +24,7 @@ async def on_member_join(member):
 
 	if member.guild.id == 799680125024337950:
 		time.sleep(610)
+		
 		# 用意したIDから Role オブジェクトを取得
 		role = member.guild.get_role(844138721200439338)
 		# 入ってきた Member に役職を付与
