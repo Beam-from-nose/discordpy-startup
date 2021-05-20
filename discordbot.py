5aba2ca4337cdcc3021bb28a79b0b3b4c154b879
Beam-from-nose <75787902+Beam-from-nose@users.noreply.github.com>
2021-05-20 21:53:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,7 +22,7 @@ async def on_command_error(ctx, error):
 async def on_member_join(member):
 
 	if member.guild.id == 799680125024337950:
-		
+		time.sleep(610)
 		# 用意したIDから Role オブジェクトを取得
 		role = member.guild.get_role(844138721200439338)
 		# 入ってきた Member に役職を付与
@@ -133,10 +133,6 @@ async def show(ctx):
 
 @bot.event
 async def on_reaction_add(reaction, user):
-	
-	#初心者権限付与
-	test = reaction.message
-	print(test.channel.id)
 	
 	#スタート以外はスルー
 	if reaction.emoji == "▶":
